d2731b545e7e2ac33441bd55c328544a8d236d51
Enzo E. Galletta <egalletta12@gmail.com>
2020-12-06 17:08:04 -0500

    no chromium sandbox

--- scraper/src/scrape.ts
+++ scraper/src/scrape.ts
@@ -84,7 +84,10 @@ const getStoriesFromPage = async (page: puppeteer.Page, pageNumber: number) => {
 };
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox"],
+  });
   const page = await browser.newPage();
   for (let i = 1; i <= 13; i++) {
     await getStoriesFromPage(page, i);
